3c9e1e2fe5b692f01f32f9963789f5642c4686b7
WingSurfSki <kubischta@gmail.com>
2023-01-01 11:54:13 -0800

    Initial Commit

---
+++ TestAppwGitHub/TestAppwGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppwGitHubApp.swift
+//  TestAppwGitHub
+//
+//  Created by DUANE KUBISCHTA on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TestAppwGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
